9aba518ec8a8bddf1061a57cd7f21fb955f66f21
skelanimals <zhangcong@9linktech.com>
2025-10-30 18:17:02 +0800

    chore: release v0.0.2

--- scripts/release.ts
+++ scripts/release.ts
@@ -13,6 +13,6 @@ if (oldVersion === version) {
   process.exit()
 }
 
-execSync('git add .', { stdio: 'inherit' }) 
+execSync('git add .', { stdio: 'inherit' })
 execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' })
 execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
