219083a175b8a98f6f1a358231cd43927cdb666d
cmtlyt <cmt01964896@taobao.com>
2025-10-31 12:44:48 +0800

    feat: 补充可选链

--- packages/render/src/hydrates/react.ts
+++ packages/render/src/hydrates/react.ts
@@ -10,7 +10,7 @@ export async function hydrate(
   if (type === 'csr') {
     // 开发环境 vite 并不会处理 commonjs 模块, 所以做一个兜底逻辑
     const module = await import('react-dom/client')
-    const createRoot = module.createRoot || module.default.createRoot
+    const createRoot = module.createRoot || module.default?.createRoot
 
     const root = createRoot(document.querySelector('#app')!)
 
